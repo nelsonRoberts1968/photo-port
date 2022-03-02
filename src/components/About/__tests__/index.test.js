bff5c376c010017d0da4b087862a4f5a26520dd0
Nelson Roberts <nellyroberts1968@gmail.com>
2022-03-01 16:09:46 -0800

    added nav

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render ,cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+
+describe('About Component', ()=>{
+    //renders About test
+
+    //first test
+    it('renders',()=>{
+        render(<About/>)
+    });
+
+    //second test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+      })
+     
+})
